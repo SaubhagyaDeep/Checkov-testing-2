bd8189dfbf02063db145116e616e149cebfdcb64
SaubhagyaDeep <91832218+SaubhagyaDeep@users.noreply.github.com>
2025-11-17 18:40:42 +0530

    test: after correcting the failures

--- terraform/environments/dev/networks/firewall.tf
+++ terraform/environments/dev/networks/firewall.tf
@@ -1,7 +1,7 @@
 resource "google_compute_firewall" "allow_all" {
   name          = "terragoat-${var.environment}-firewall"
   network       = google_compute_network.vpc.id
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["125.53.43.12/32"]
   allow {
     protocol = "tcp"
     ports    = ["0-1000"]
